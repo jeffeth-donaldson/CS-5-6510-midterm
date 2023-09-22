931eeb8f61c689758f2b5ef3900c1ebcc6439ed1
Bridget Staley <lynnbridgegirl@gmail.com>
2023-09-22 12:35:48 -0600

    Planned path for driving

--- Q2.py
+++ Q2.py
@@ -4,8 +4,10 @@ import matplotlib.patches as patches
 
 VELOCITY = 8
 LENGTH = 10.668
-# REFRESH_RATE = 0.5 #in seconds
-REFRESH_RATE = 0.01 #in seconds
+REFRESH_RATE = 0.5 #in seconds
+# REFRESH_RATE = 0.01 #in seconds
+IDEAL_REFRESH = .01
+RADIUS = 18 # 18 meters
 
 class Car():
     def __init__(self, x_init, y_init, alpha_init):
@@ -20,24 +22,38 @@ class Car():
 
     def drive(self, duration, alpha=0):
         for _ in range(int(duration/REFRESH_RATE)):
-                omega:float = VELOCITY / LENGTH * np.tan(alpha)
-                theta_next = self.theta_hist[-1] + omega * REFRESH_RATE
-                self.theta_hist.append(theta_next)
+            omega:float = VELOCITY / LENGTH * np.tan(alpha)
+            theta_next = self.theta_hist[-1] + omega * REFRESH_RATE
+            self.theta_hist.append(theta_next)
 
-                x_vel = VELOCITY * np.sin(self.theta_hist[-1])
-                y_vel = VELOCITY * np.cos(self.theta_hist[-1])
+            x_vel = VELOCITY * np.sin(self.theta_hist[-1])
+            y_vel = VELOCITY * np.cos(self.theta_hist[-1])
 
-                x_next = self.x_hist[-1] + x_vel * REFRESH_RATE
-                y_next = self.y_hist[-1] + y_vel * REFRESH_RATE
+            x_next = self.x_hist[-1] + x_vel * REFRESH_RATE
+            y_next = self.y_hist[-1] + y_vel * REFRESH_RATE
 
-                self.x_hist.append(x_next)
-                self.y_hist.append(y_next)
+            self.x_hist.append(x_next)
+            self.y_hist.append(y_next)
+
+    def getPath(self, radius, start, end):
+        # Calculate circle
+        cir = 2 * np.pi * radius
+        theta = end - start
+        # Divide into frame-long arcs
+        dist = cir * theta / (2*np.pi)
+        segments = np.floor(dist / VELOCITY)
+        # Get array of ideal points
+
+        # Find theta for each of the points
+
+        # Return theta array
+
 
     def plot(self):
         fig, ax = plt.subplots()
 
         #Plot the boundry circle 
-        circle = patches.Circle((0, 0), radius=18, fill=False, edgecolor='r', linewidth=3, linestyle='dotted')
+        circle = patches.Circle((0, 0), radius=RADIUS, fill=False, edgecolor='r', linewidth=3, linestyle='dotted')
         ax.add_patch(circle)
         ax.set_xlim(-20, 20)
         ax.set_ylim(-20, 20)
@@ -51,10 +67,15 @@ class Car():
 
 
 if __name__ == '__main__':
-     # Part 1 
-     p1Car = Car(0, 0, 0)
+    # Part 1 
+    p1Car = Car(0, 0, 0)
 
-     p1Car.drive((np.pi * 9) / VELOCITY, np.arctan(LENGTH / 9))
-     p1Car.drive(15, np.arctan(LENGTH / 18))
+    # Get to edge of circle
+    p1Car.getPath(())
+    # Loop through thetas on the path and drive for 1 frame each
+    p1Car.drive((np.pi * 9) / VELOCITY, np.arctan(LENGTH / 9))
+    # Drive along circle
 
-     p1Car.plot()
+    p1Car.drive(15, np.arctan(LENGTH / RADIUS))
+
+    p1Car.plot()
